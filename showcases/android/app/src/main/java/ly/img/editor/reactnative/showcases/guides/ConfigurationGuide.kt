55ec8efb6ba340b2f243b75276624c42ee2cf55b
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-26 15:20:21 +0200

    Update examples for v1.52.0-rc.1

--- showcases/android/app/src/main/java/ly/img/editor/reactnative/showcases/guides/ConfigurationGuide.kt
+++ showcases/android/app/src/main/java/ly/img/editor/reactnative/showcases/guides/ConfigurationGuide.kt
@@ -2,22 +2,22 @@
 
 package ly.img.editor.reactnative.showcases.guides
 
-// #highlight-import-compose
+// highlight-import-compose
 import androidx.compose.runtime.Composable
-// #highlight-import-compose
+// highlight-import-compose
 import ly.img.camera.core.CaptureVideo
 import ly.img.camera.core.EngineConfiguration
 import ly.img.camera.reactnative.module.IMGLYCameraModule
 import ly.img.camera.reactnative.module.model.CameraResult
-// #highlight-import-editor
+// highlight-import-editor
 import ly.img.editor.reactnative.module.IMGLYEditorModule
 import ly.img.editor.reactnative.module.builder.EditorBuilder
 import ly.img.editor.reactnative.module.builder.EditorBuilderResult
 import ly.img.editor.reactnative.module.model.EditorSettings
-// #highlight-import-editor
+// highlight-import-editor
 
 private fun useCustomEditor() {
-    // #highlight-closure
+    // highlight-closure
     IMGLYEditorModule.builderClosure = { _, metadata ->
         if (metadata?.get("custom") == true) {
             EditorBuilder.custom {
@@ -27,26 +27,26 @@ private fun useCustomEditor() {
             EditorBuilder.design()
         }
     }
-    // #highlight-closure
+    // highlight-closure
 }
 
 private fun customizeCamera() {
-    // #highlight-camera-configuration
-    // #highlight-camera-configuration-closure
+    // highlight-camera-configuration
+    // highlight-camera-configuration-closure
     // Configure the [CaptureVideo.Input].
     IMGLYCameraModule.configurationClosure = { metadata ->
         val engineConfiguration = EngineConfiguration("MY_LICENSE")
         CaptureVideo.Input(engineConfiguration)
     }
 
-    // #highlight-camera-configuration-closure
-    // #highlight-camera-result-closure
+    // highlight-camera-configuration-closure
+    // highlight-camera-result-closure
     // Modify the [CameraResult].
     IMGLYCameraModule.resultClosure = { result ->
         CameraResult(result?.recording, mapOf("MY_CUSTOM_KEY" to "MY_CUSTOM_VALUE"))
     }
-    // #highlight-camera-result-closure
-    // #highlight-camera-configuration
+    // highlight-camera-result-closure
+    // highlight-camera-configuration
 }
 
 @Composable
