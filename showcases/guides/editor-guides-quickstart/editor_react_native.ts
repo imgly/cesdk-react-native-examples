cb9a736861257f10ba6ca00c36eee13d142d4807
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-11-19 00:52:04 +0100

    Update examples for v1.64.0-rc.2

--- showcases/guides/editor-guides-quickstart/editor_react_native.ts
+++ showcases/guides/editor-guides-quickstart/editor_react_native.ts
@@ -2,7 +2,7 @@ import IMGLYEditor, { EditorSettingsModel } from '@imgly/editor-react-native';
 
 export const apparel_editor_solution = async (): Promise<void> => {
   // highlight-configuration
-  const settings = new EditorSettingsModel({ license: 'YOUR_LICENSE_KEY' });
+  const settings = new EditorSettingsModel({ license: 'YOUR_LICENSE_KEY' }); // Get your license from https://img.ly/forms/free-trial, pass null for evaluation mode with watermark
   // highlight-configuration
   // highlight-editor
   const result = await IMGLYEditor?.openEditor(settings);
