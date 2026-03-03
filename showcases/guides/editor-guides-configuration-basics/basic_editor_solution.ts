dfbf4b5ace37a2174258b31f51711905564e0dca
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-03-03 08:47:36 +0100

    Update examples for v1.70.0-rc.2

--- showcases/guides/editor-guides-configuration-basics/basic_editor_solution.ts
+++ showcases/guides/editor-guides-configuration-basics/basic_editor_solution.ts
@@ -9,12 +9,9 @@ export const basicEditor = async (): Promise<void> => {
     // highlight-license
     license: 'YOUR_LICENSE_KEY', // Get your license from https://img.ly/forms/free-trial, pass null for evaluation mode with watermark
     // highlight-license
-    // highlight-sceneBaseUri
-    sceneBaseUri: 'YOUR_SCENE_BASE_URI',
-    // highlight-sceneBaseUri
-    // highlight-assetBaseUri
-    assetBaseUri: 'YOUR_ASSET_BASE_URI',
-    // highlight-assetBaseUri
+    // highlight-baseUri
+    baseUri: 'YOUR_BASE_URI',
+    // highlight-baseUri
     // highlight-userId
     userId: 'YOUR_USER_ID'
     // highlight-userId
