0a38235e9f0b1f48bec8d6e517a5523cd72d35c4
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-26 11:37:41 +0000

    Update examples for v1.81.1

--- showcases/metro.config.js
+++ showcases/metro.config.js
@@ -1,5 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('scene'); // Add your custom extension here
+config.resolver.assetExts.push('scene', 'imgly'); // Add your custom extension here
 module.exports = config;
